84a04c07d386f9677934c779cc316eb38eeee76e
Michael Caballero <michaelcaballero@Michaels-MacBook-Pro.local>
2022-02-05 22:18:44 -0800

    Update company contract

--- hardhat/contracts/Company.sol
+++ hardhat/contracts/Company.sol
@@ -10,42 +10,56 @@ contract Company is Ownable {
     using Math for uint;
     using SafeERC20 for IERC20;
 
-    address public paymentToken;
+    event stopPaying(address _employee, address _payrollLocation);
 
+    address public paymentToken;
     // Amount of times an employee gets paid a year
-    mapping(address=>uint) public salaryFreq;
+    uint public salaryFreq;
     //Amount that an employee gets paid each year
     mapping(address=>uint) public salaryAmt;
+    mapping(address=>bool) public employed;
+    mapping(address=>address) public payrollAddress;
 
     function changePaymentToken(address _token) external onlyOwner {
         paymentToken = _token;
     }
 
-    function newEmployee(address _employee, uint _freq, uint _amt) external onlyOwner {
-        salaryFreq[_employee] = _freq;
-        salaryFreq[_employee] = _amt;
+    function changePayrollFreq(uint _freq) external onlyOwner {
+        salaryFreq = _freq;
+    }
+
+    function newEmployee(address _employee, uint _amt) external onlyOwner {
+        salaryAmt[_employee] = _amt;
+        employed[_employee] = true;
+        payrollAddress[_employee] = _employee;
+    } 
+
+    function changeAmt(address _employee, uint _amt) external onlyOwner {
+        require(employed[_employee], 'Must be employed by the company.');
+        salaryAmt[_employee] = _amt;
     } 
 
     function deposit(address _token, uint _amount) external {
-        require(_token==paymentToken, "Payment system can only accept DAIx." );
+        require(_token==paymentToken, "Payroll can only accept the official payment token." );
         IERC20(paymentToken).safeTransferFrom(msg.sender, address(this), _amount);
     }
 
     function payEmployee(address _employee) external onlyOwner {
-        IERC20(paymentToken).safeTransfer(_employee, Math.ceilDiv(salaryAmt[_employee], salaryFreq[_employee]));
+        IERC20(paymentToken).safeTransfer(payrollAddress[_employee], Math.ceilDiv(salaryAmt[_employee], salaryFreq));
     }
 
-    function changeEmployeeAddress(address _former, address _new) external {
-        require(msg.sender==_former || msg.sender==owner(), 
-            'Only employer or specific employee can change their payment location.');
-        salaryFreq[_new] = salaryFreq[_former];
-        salaryAmt[_new] = salaryAmt[_former]; 
-        delete salaryFreq[_former];
-        delete salaryAmt[_former];
+    function changeEmployeeAddress(address _new) external {
+        require(payrollAddress[msg.sender]!=address(0x0000000000000000000000000000000000000000), 
+            "Can't change payroll address if that employee doesn't have a payroll address.");
+        emit stopPaying(msg.sender, payrollAddress[msg.sender]);
+        payrollAddress[msg.sender] = _new;
     }
 
     function fireEmployee(address _employee) external onlyOwner {
-        delete salaryFreq[_employee];
+        require(employed[_employee], 'Must be employed by the company.');
+        emit stopPaying(_employee, payrollAddress[_employee]);
+        employed[_employee] = false;
         delete salaryAmt[_employee];
+        delete payrollAddress[_employee];
     }
 }

2fce0fa4a502ee6f8c7bb61a82805b0f546b5a37
Michael Caballero <michaelcaballero@Michaels-MacBook-Pro.local>
2022-02-05 17:28:29 -0800

    Add company contract

---
+++ contracts/company.sol
@@ -0,0 +1,47 @@
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/utils/math/Math.sol";
+
+contract Company is Ownable {
+
+    using SafeMath for uint;
+    using SafeERC20 for IERC20;
+
+    address public paymentToken;
+
+    // Amount of times an employee gets paid a year
+    mapping(address=>uint) public salaryFreq;
+    //Amount that an employee gets paid each year
+    mapping(address=>uint) public salaryAmt;
+
+    function ChangePaymentToken(address _token) external onlyOwner {
+        paymentToken = _token;
+    }
+
+    function SalaryFrequence(address _employee) public view returns (uint) {
+        return salaryFreq[_employee];
+    } 
+
+    function SalaryAmount(address _employee) public view returns (uint) {
+        return salaryAmt[_employee];
+    } 
+
+    function ChangeSalaryFrequence(address _employee, uint _freq) external onlyOwner {
+        salaryFreq[_employee] = _freq;
+    } 
+
+    function ChangeSalaryAmount(address _employee, uint _amt) external onlyOwner {
+        return salaryFreq[_employee] = _amt;
+    } 
+
+    function Deposit(address _token, uint _amount) external {
+        require(_token==paymentToken, "Payment system can only accept USDC." );
+        IERC20(_token).safeTransferFrom(msg.sender, address(this), _amount);
+    }
+
+    function PayEmployee(address _employee) external onlyOwner {
+        IERC20(_token).safeTransfer(paymentToken, _employee, ceilDiv(salaryAmt, salaryFreq));
+    }
+}
